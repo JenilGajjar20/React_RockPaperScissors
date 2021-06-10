398925fc9acc90b26c3d3781fc3e1126645bacb3
Jenil Gajjar <jenilgajjar@gmail.com>
2021-06-10 14:18:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const choices = ["rock", "paper", "scissors"];
+
+  const handleClick = (value) => {
+    setUserChoice(value);
+    generateComputerChoice();
+  };
+
+  const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+    // console.log(randomChoice);
+  };
+
+  useEffect(() => {
+    switch (userChoice + computerChoice) {
+      case "scissorspaper":
+      case "rockscissors":
+      case "paperrock":
+        setResult("You Win!🎉");
+        break;
+      case "paperscissors":
+      case "scissorsrock":
+      case "rockpaper":
+        setResult("You Lose!😥");
+        break;
+      case "paperpaper":
+      case "rockrock":
+      case "scissorsscissors":
+        setResult("It's a Draw!😃");
+        break;
+    }
+  }, [userChoice, computerChoice]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Follow me on: <a href="https://github.com/JenilGajjar20" target="_blank">GitHub</a></p>
+      <div className="choices">
+        <h2>
+          User Choice: <span className="userChoice">{userChoice}</span>
+        </h2>
+        <h2>
+          Computer Choice:{" "}
+          <span className="computerChoice">{computerChoice}</span>
+        </h2>
+      </div>
+      {choices.map((choice, index) => (
+        <button key={index} onClick={() => handleClick(choice)}>
+          {choice}
+        </button>
+      ))}
+      <div className="result">
+        <h3>
+          {result === "You Win!🎉" ? (
+            <span className="win">{result}</span>
+          ) : result === "You Lose!😥" ? (
+            <span className="lose">{result}</span>
+          ) : (
+            <span className="draw">{result}</span>
+          )}
+        </h3>
+      </div>
     </div>
   );
 }
